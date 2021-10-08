7903c3b3c8e88d3b84270cd3719ae0c5e9552781
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-08 22:47:10 +0530

    add a few more changes

--- max-heap.py
+++ max-heap.py
@@ -24,11 +24,37 @@ def max_heapify(arr: List[int], heap_size: int, i: int):
     arr[i] = temp
     max_heapify(arr, heap_size, largest)
 
+
+def max_heapify_it(arr: List[int], heap_size: int, i: int):
+  
+  largest = -1
+
+  while True:
+    l = left(i)
+    r = right(i)
+    if l < heap_size and arr[l] > arr[i]:
+      largest = l
+    else:
+      largest = i
+
+    if r < heap_size and arr[r] > arr[largest]:
+      largest = r
+    
+    if largest != i:
+      temp = arr[largest]
+      arr[largest] = arr[i]
+      arr[i] = temp
+      i = largest
+    else: 
+      break
+
+
 def build_max_heap(arr: List[int]):
   heap_size = len(arr)
   for i in range(heap_size//2 - 1, -1, -1):
-    max_heapify(arr, heap_size,i)
+    max_heapify_it(arr, heap_size,i)
   
-  print(arr)
+  return arr
+
 
 print(build_max_heap([1, 2, 3, 4, 5, 6]))
