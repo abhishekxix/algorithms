befb0294f2b4e35bdb3a02655c2a6c273916f4a5
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-11 18:51:25 +0530

    Refactor code

--- heapsort.py
+++ heapsort.py
@@ -6,9 +6,7 @@ def heapsort(arr: List[int]):
   max_heap.build_max_heap(arr)
 
   for i in range(len(arr) - 1, 0, -1):
-    temp = arr[0]
-    arr[0] = arr[i]
-    arr[i] = temp
+    arr[i], arr[0] = arr[0], arr[i]
     heap_size -= 1
     max_heap.max_heapify_it(arr, heap_size, 0)
 
