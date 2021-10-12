f54336563529869e4bd5088c316c492bf62fc167
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-12 17:52:01 +0530

    add quickselect

---
+++ quickselect.py
@@ -0,0 +1,39 @@
+import random
+
+
+def partition(a, p, r):
+  x = a[r]
+  i = p - 1
+
+  for j in range(p, r):
+    if a[j] < x:
+      i += 1
+      a[i], a[j] = a[j], a[i]
+
+  i += 1
+  a[r], a[i] = a[i], a[r]
+  return i
+
+
+def random_partition(a, p, r):
+  q = random.randint(p, r)
+  a[q], a[r] = a[r], a[q]
+  return partition(a, p, r)
+
+
+def quickselect(a, p, r, i):
+  if p == r:
+    return a[p]
+  q = random_partition(a, p, r)
+  k = q - p + 1
+
+  if k == i:
+    return a[k]
+
+  if i < k:
+    return quickselect(a, p, q - 1, i)
+
+  return quickselect(a, q + 1, r, i - k)
+
+
+print(quickselect([12, 3, 5, 7, 19], 0, 4, 5))
