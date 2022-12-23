ed6ae31db5afa1c611f0652fac985cb90af72ead
Abhishek Singh <abhisheksinghxix@gmail.com>
2022-12-23 23:58:50 +0530

    BST: remove

--- linkedlist/bst.cpp
+++ linkedlist/bst.cpp
@@ -50,6 +50,60 @@ class BST {
     return potentialParent;
   }
 
+  void replaceNode(Node* original, Node* replacement) {
+    Node* oParent = original->getParent();
+    Node* oLeft = original->getLeft();
+    Node* oRight = original->getRight();
+
+    if (oParent) {
+      if (oParent->getLeft() == original)
+        oParent->setLeft(replacement);
+      else
+        oParent->setRight(replacement);
+    } else {
+      root = replacement;
+    }
+
+    if (replacement) {
+      Node* rParent = replacement->getParent();
+      Node* rRight = replacement->getRight();
+
+      // getting replacement out of place
+      if (/* rRight and  */ rParent != original) {
+        if (rRight) rRight->setParent(rParent);
+
+        if (rParent->getLeft() == replacement)
+          rParent->setLeft(rRight);
+        else
+          rParent->setRight(rRight);
+
+        replacement->setRight(oRight);
+        if (oRight) oRight->setParent(replacement);
+      }
+
+      // assigning left child to replacement
+      if (replacement != oLeft) {
+        replacement->setLeft(oLeft);
+        if (oLeft) oLeft->setParent(replacement);
+      }
+
+      // assigning right child to replacement
+      // if (rParent != original) {
+      //   replacement->setRight(oRight);
+      //   if (oRight) oRight->setParent(replacement);
+      // }
+
+      // assigning parent to replacement
+      replacement->setParent(oParent);
+    }
+  }
+
+  Node* getSmallestDescendant(Node* curr) {
+    if (!curr->getLeft()) return curr;
+
+    return getSmallestDescendant(curr->getLeft());
+  }
+
  public:
   BST(int data) : root{new Node(data)}, size{0} {}
   BST() : root{nullptr}, size{0} {}
@@ -104,6 +158,20 @@ class BST {
     std::cout << currNode->getData() << " ";
     inorderTraversal(currNode->getRight());
   }
+
+  void remove(int val) {
+    Node* toDelete = find(root, val);
+    size--;
+
+    if (!toDelete) return;
+
+    if (toDelete->getRight())
+      replaceNode(toDelete, getSmallestDescendant(toDelete->getRight()));
+    else
+      replaceNode(toDelete, toDelete->getLeft());
+
+    delete toDelete;
+  }
 };
 
 int main() {
@@ -121,6 +189,14 @@ int main() {
   bst.insert(7);
   bst.insert(10);
 
+  // bst.remove(5);
+  // bst.remove(10);
+  // bst.remove(8);
+  // bst.remove(3);
+
+  while (bst.getSize()) {
+    bst.remove(bst.getRoot()->getData());
+  }
   bst.inorderTraversal(bst.getRoot());
 
   return 0;
